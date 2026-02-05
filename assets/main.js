3e229efa542712d7b2d6907e2e951e0024aee1fb
Peter Jiang <daseal211@gmail.com>
2026-02-05 14:24:24 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,12 @@
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.classList.add('active');
+  });
 
+  card.addEventListener('mouseleave', () => {
+    card.classList.remove('active');
+  });
+});
+
+const status = document.querySelector('.status-card p');
+status.textContent = "Test 2";
